aa8e7339da3a471f2b8bc43d33f04123dd9ac69a
Jeff <jmills@delphicdigital.com>
2016-08-31 16:33:02 -0400

    Add a skull to beginning of each console message

---
+++ skeletor.debug.js
@@ -0,0 +1,12 @@
+var origConsole = window.console;
+
+window.console = new Proxy(origConsole, {
+	get: function(target, property, receiver) {
+		var origMethod = target[property];
+		return function () {
+			var args = Array.prototype.slice.call(arguments)
+			var result = origMethod.apply(origConsole, ['%c💀','color:#e3c376'].concat(args));
+			return result;
+		};
+	}
+});
